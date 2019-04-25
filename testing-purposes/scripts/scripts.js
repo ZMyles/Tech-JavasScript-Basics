bb4e4f5ef5151528f466b798d4a584e40955dc27
Zavier <zmyles@comcast.net>
2019-04-25 15:28:34 -0400

    testing loops, file under Testing-purposes

---
+++ testing-purposes/scripts/scripts.js
@@ -0,0 +1,63 @@
+"use strict"
+
+
+
+
+// for(let i = 1; i <= 1000; i ++){
+//   console.log(i)
+// }
+
+
+for(let i = 0; i <= 10000; i++){
+  if( i % 2 == 0){
+    console.log(i);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ================================================
+//     SWITCH STATMENTS
+// ==============================================
+
+
+
+// var randomNumber = Math.floor((Math.random()*10));
+// // var randomNumber = 11;
+// switch (randomNumber) {
+//   case 0:
+//   case 1:
+//   case 2:
+//   console.log(`The random Number is ${randomNumber}.`);
+//   // break;
+//   case 3:
+//   case 4:
+//   case 5:
+//   case 6:
+//   console.log(`The random Number is Between 3 and 7.`);
+//   // break;
+//   case 7:
+//   case 8:
+//   case 9:
+//   case 10:
+//   console.log(`the random number is greater than 7.`);
+//   // break;
+//   default:
+//   console.log(`Something went wrong...`);
+// }
