570d88f9c0b2ed98121093dfd3034b14f6fe7806
Zavier <zmyles@comcast.net>
2019-04-26 17:45:29 -0400

    interacting with HTML with JavaScript

---
+++ basic-JavaScript2/scripts/script.js
@@ -0,0 +1,31 @@
+"use strict"
+
+// document.getElementById("header").innerHTML = "Hellow World";
+
+// var lang = "JavaScript";
+
+// function myFunction(lang){
+//   this.lang = lang;
+//   console.log("hello, Welcome to " + this.lang);
+// };
+
+// var Test = new myFunction(lang)
+
+
+// var myCar1 = new Object();
+
+// myCar1.make = "Ford";
+// myCar1.Model = "Mustage";
+
+// console.log(myCar1.make);
+// console.log(myCar1["Model"])
+
+
+// function myFunction(make, model){
+//   this.make = make;
+//   this.model = model;
+//   document.write(this.make + " : " + this.model);
+// }
+
+// var myCar1 = new myFunction("Ford", "Mustang");
+// var myCar2 = new myFunction("Ford", "F15");
